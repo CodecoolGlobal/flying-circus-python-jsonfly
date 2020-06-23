550cec8ae3741b38fba207e730e93d8948ceddee
Gábor Hajdu <gabor.hajdu@codecool.com>
2020-06-23 10:42:04 +0200

    add starter flask code

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import data
 
-# put your code here
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    # Return this to the user who visited this page. The browser will render it.
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
